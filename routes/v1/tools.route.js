2a130eb7197edc0e93b8c4b8a6cce74b1f1c39d8
mollikacomputer <mollikacomputer3@gmail.com>
2022-09-01 20:24:44 +0600

    post delete put testing done

--- routes/v1/tools.route.js
+++ routes/v1/tools.route.js
@@ -6,7 +6,14 @@ router
 .get((req, res) =>{
     res.send('Test tools route get option');
 })
-.post()
-.delete();
+.post((req, res) => {
+    res.send('tools added');
+})
+.delete( (req, res)=>{
+    res.send(' deleted data from  tools')
+})
+.put((req, res)=>{
+    res.send('put option added')
+});
 
 module.exports = router;
