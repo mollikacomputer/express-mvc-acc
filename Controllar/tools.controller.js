5d830c322d9095232eec751d0612fb0ce5d3c975
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-06-27 04:33:25 +0600

    query parameter example  added

--- Controllar/tools.controller.js
+++ Controllar/tools.controller.js
@@ -1,5 +1,15 @@
+const tools = [
+    {_id:1, name: "Hammer"},
+    {_id:2, name: "Trolly"},
+    {_id:3, name: "Scrue Driver"},
+    {_id:4, name: "Cutter"},
+    {_id:5, name: "Tester"},
+];
+
 module.exports.getAllTools = (req, res, next) =>{
-    res.send('Tools Found');
+    const {limit, page} = req.query;
+    console.log(limit, page);
+    res.send(tools);
 }
 
 module.exports.AddATools = (req, res, next) => {
--- Controllar/user.controller.js
+++ Controllar/user.controller.js
@@ -1,5 +1,16 @@
+const tools = [
+    {_id:1, name: "Ram"},
+    {_id:2, name: "Rahim"},
+    {_id:3, name: "Tiku"},
+    {_id:4, name: "Piku"},
+    {_id:5, name: "Sejuti"},
+];
+
 module.exports.getAllUser = (req, res, next) =>{
-    res.send('Found All User');
+    // write a query with url ?limit=10&page=3
+    const {limit, page} = req.query;
+    console.log(limit, page);
+    res.send(tools);
 }
 
 module.exports.AddAUser = (req, res, next) => {
